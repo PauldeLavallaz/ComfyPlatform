aede96d2289d58f9aae49ce41484ddd0e4518a57
Paul de Lavallaz <pauldelavallaz@hotmail.com>
2024-12-12 03:45:57 -0300

    last

--- src/app/lujan-tech/page.tsx
+++ src/app/lujan-tech/page.tsx
@@ -14,12 +14,13 @@ export default function LujanTechDayPage() {
 
   return (
     <div className="flex flex-col h-screen">
-      <div className="fixed top-0 left-0 right-0 z-10 bg-white">
-        <h1 className="text-2xl font-bold px-6 py-2 border-b flex-none md:ml-64">
+      <div className="fixed top-0 left-0 right-0 z-20 bg-white">
+        <div className="h-16" />
+        <h1 className="text-2xl font-bold px-6 py-2 border-b flex-none md:ml-64 bg-white">
           Luján Tech
         </h1>
       </div>
-      <div className="flex-1 overflow-hidden mt-[44px]">
+      <div className="flex-1 overflow-hidden pt-[88px]">
         <GeneratorLayout inputs={inputs}>
           <UserRuns deploymentId="4bec08ac-4e1b-4ada-bd79-19a1fab8158a" />
         </GeneratorLayout>
